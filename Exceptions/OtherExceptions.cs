e8c6009c7982d9399f8e6457d84ba0d05c343463
piyush <piyusheverhard@gmail.com>
2023-05-28 00:31:09 +0530

    Added Exceptions for Exception Handling

---
+++ Exceptions/OtherExceptions.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Diskode.Exceptions;
+public class RepositoryException : Exception
+{
+    public RepositoryException(string message, Exception innerException)
+    : base(message, innerException)
+    {
+    }
+}
+
+public class DuplicateEmailException : Exception
+{
+    public DuplicateEmailException(string message, Exception innerException)
+    : base(message, innerException)
+    {
+    }
+}
+
