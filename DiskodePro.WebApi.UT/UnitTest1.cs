fc49a34600cd70dd7ec42875c83c45582c8b9190
Piyush Kumar <piyusheverhard@gmail.com>
2023-09-16 11:27:08 +0530

    Added WebApi Unit Test Project.

---
+++ DiskodePro.WebApi.UT/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DiskodePro.WebApi.UT;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
